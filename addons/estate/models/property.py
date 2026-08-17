91c45c11761c008f52c1a699c44460e53818b573
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-17 16:57:02 -0400

    refactor(property): update date_availability field and improve field definitions

--- addons/estate/models/property.py
+++ addons/estate/models/property.py
@@ -8,10 +8,12 @@ class Property(models.Model):
     name = fields.Char(string="Name of the property", required=True)
     description = fields.Text(string="Property description")
     postcode = fields.Char(string="Pastal code")
-    date_availability = fields.Date(string="Availability date")
+    # TODO: Default availability date must be 3 months after current date
+    #  https://www.odoo.com/documentation/19.0/es_419/developer/tutorials/server_framework_101/05_firstui.html#fields-attributes-and-view
+    date_availability = fields.Date(string="Availability date", copy=False, default=)
     expected_price = fields.Float(string="Expected price", required=True)
-    selling_price = fields.Float(string="Selling price")
-    bedrooms = fields.Integer(string="Amount of bedroom")
+    selling_price = fields.Float(string="Selling price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Amount of bedroom", default=2)
     living_area = fields.Integer(string="Amount of livings")
     facades = fields.Integer(string="Amoount of facades")
     garage = fields.Boolean(string="Have garage?")
