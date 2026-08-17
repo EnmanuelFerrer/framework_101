97368064ff33664c0c1be62dd4d739cb4cdbe2a2
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-17 16:16:06 -0400

    refactor(estate): translate fields from Spanish to English

--- addons/estate/models/property.py
+++ addons/estate/models/property.py
@@ -3,28 +3,26 @@ from odoo import fields, models
 
 class Property(models.Model):
     _name = "property"
-    _description = "Propiedad"
+    _description = "Property"
 
-    name = fields.Char(string="Nombre de la propiedad", required=True)
-    description = fields.Text(string="Descripcion de la propiedad", required=False)
-    postcode = fields.Char(
-        string="Codigo postal", required=True, description="Codigo postal"
-    )
-    date_availability = fields.Date(string="Fecha de disponibilidad")
-    expected_price = fields.Float(string="Precio", required=True)
-    selling_price = fields.Float(string="Precio de venta")
-    bedrooms = fields.Integer(string="Cantidad de habitaciones")
+    name = fields.Char(string="Name of the property", required=True)
+    description = fields.Text(string="Property description")
+    postcode = fields.Char(string="Pastal code")
+    date_availability = fields.Date(string="Availability date")
+    expected_price = fields.Float(string="Expected price", required=True)
+    selling_price = fields.Float(string="Selling price")
+    bedrooms = fields.Integer(string="Amount of bedroom")
     living_area = fields.Integer(string="Cantidad de livings")
-    facades = fields.Integer(string="Cantidad de fachadas")
-    garage = fields.Boolean(string="Tiene garage")
-    garden = fields.Boolean(string="Tiene jardin")
-    garden_area = fields.Integer(string="Tamanio del jardin")
+    facades = fields.Integer(string="Amoount of facades")
+    garage = fields.Boolean(string="Have garage?")
+    garden = fields.Boolean(string="Have garden?")
+    garden_area = fields.Integer(string="Garden area")
     garden_orientation = fields.Selection(
-        string="Orientacion cardinal del jardin",
+        string="Cardinal orientation of the garden",
         selection=[
-            ("Norte", "north"),
-            ("Sur", "south"),
-            ("Este", "east"),
-            ("Oeste", "West"),
+            ("North", "north"),
+            ("South", "south"),
+            ("East", "east"),
+            ("Weste", "west"),
         ],
     )
