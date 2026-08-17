128dc7977bda56f3466427e6577cda0f6f3fdca7
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-17 16:16:37 -0400

    feat(estate): add menu structure and register views in manifest

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -14,8 +14,8 @@
         "security/ir.model.access.csv",
 
         # Views
-        "views/estate_property_views.xml"
-        # "views/estate_menus.xml"
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml"
     ],
     "application": False,
     "installable": True,
