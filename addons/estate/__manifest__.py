58e220fe3ab42b96a9f931f91ca7c89e9d65408f
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-17 15:42:11 -0400

    feat(estate_property_view): add view file

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -12,6 +12,10 @@
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
+
+        # Views
+        "views/estate_property_views.xml"
+        # "views/estate_menus.xml"
     ],
     "application": False,
     "installable": True,
