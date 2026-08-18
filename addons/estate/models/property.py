3c6c9643108ed4a0faa39a3e8152f1fca2683496
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-18 09:26:15 -0400

    fix(property): correct spelling in facades field label and remove default value from date_availability

--- addons/estate/models/property.py
+++ addons/estate/models/property.py
@@ -10,12 +10,12 @@ class Property(models.Model):
     postcode = fields.Char(string="Pastal code")
     # TODO: Default availability date must be 3 months after current date
     #  https://www.odoo.com/documentation/19.0/es_419/developer/tutorials/server_framework_101/05_firstui.html#fields-attributes-and-view
-    date_availability = fields.Date(string="Availability date", copy=False, default=)
+    date_availability = fields.Date(string="Availability date", copy=False)
     expected_price = fields.Float(string="Expected price", required=True)
     selling_price = fields.Float(string="Selling price", readonly=True, copy=False)
     bedrooms = fields.Integer(string="Amount of bedroom", default=2)
     living_area = fields.Integer(string="Amount of livings")
-    facades = fields.Integer(string="Amoount of facades")
+    facades = fields.Integer(string="Amount of facades")
     garage = fields.Boolean(string="Have garage?")
     garden = fields.Boolean(string="Have garden?")
     garden_area = fields.Integer(string="Garden area")
