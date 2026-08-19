e72627b2ed65b7a7f5898d8b659455d36e65621f
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-19 15:18:55 -0400

    feat(estate): add estate.property.tag model, views and menus

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -164,6 +164,8 @@ class EstateProperty(models.Model):
     )
 
     property_type_id = fields.Many2one("estate.property.type", string="Type")
+    property_tag_ids = fields.Many2many("estate.property.tag", string="Tag")
+
     salesperson_id = fields.Many2one(
         "res.users", string="Salesperson", default=lambda self: self.env.user
     )
