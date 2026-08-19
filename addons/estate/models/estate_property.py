8507c7abc1e7978b9ea7850e8c9f5fd61f57a725
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-19 16:12:09 -0400

    feat(estate): add estate.property.offer model with offers in form view

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -164,9 +164,9 @@ class EstateProperty(models.Model):
     )
 
     property_type_id = fields.Many2one("estate.property.type", string="Type")
-    property_tag_ids = fields.Many2many("estate.property.tag", string="Tag")
-
+    property_tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     salesperson_id = fields.Many2one(
         "res.users", string="Salesperson", default=lambda self: self.env.user
     )
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
