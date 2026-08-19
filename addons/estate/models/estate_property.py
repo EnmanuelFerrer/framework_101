ae0c8f8e34b7c75af4ca33aee1d526f4bb62ceaf
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-19 13:38:47 -0400

    feat(estate): add buyer and salesperson fields to estate.property

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -164,3 +164,7 @@ class EstateProperty(models.Model):
     )
 
     property_type_id = fields.Many2one("estate.property.type", string="Type")
+    salesperson_id = fields.Many2one(
+        "res.users", string="Salesperson", default=lambda self: self.env.user
+    )
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
