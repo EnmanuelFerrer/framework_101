bc8500b841de6deb8c9975e6fc980bb49b263d37
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-20 11:18:08 -0400

    feat(estate_property_offer): implement compute-inverse metod for auto/manual calculation of validiy and date_deadline

--- addons/estate/models/estate_property_offer.py
+++ addons/estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from datetime import timedelta
+
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -16,3 +18,29 @@ class EstatePropertyOffer(models.Model):
 
     buyer_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+
+    validity = fields.Integer("Validity (days)", default=7)
+    date_deadline = fields.Date(
+        string="Deadline",
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+    )
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            date_from = (
+                record.create_date.date()
+                if record.create_date
+                else fields.Date.today()
+            )
+            record.date_deadline = date_from + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            date_from = (
+                record.create_date.date() if record.create_date else fields.Date.today()
+            )
+            # Set validity if the user modifies the date_deadline.
+            if record.date_deadline:
+                record.validity = (record.date_deadline - date_from).days
