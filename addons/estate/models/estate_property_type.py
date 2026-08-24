cfc05d1c32b17e9abba275c825a452dc9b488924
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-24 12:06:12 -0400

    feat(estate): add manual ordering with sequence and handle widget

--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -4,11 +4,12 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Type"
-    _order = "name asc"
+    _order = "sequence, name"
 
     _check_name = models.Constraint("unique (name)", "Name must be unique.")
 
     name = fields.Char(string="Name", required=True)
+    sequence = fields.Integer("Sequence", default=1, help="Used to order types")
     property_ids = fields.One2many(
         "estate.property", "property_type_id", string="Properties"
     )
