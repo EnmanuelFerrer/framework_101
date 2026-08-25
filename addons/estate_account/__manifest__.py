4d2e9d90550b3fd77cc90604096fa2b1f4e55198
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-25 15:58:22 -0400

    feat(estate_account): add initial manifest and __init__ files

---
+++ addons/estate_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Estate Account",
+    "version": "19.0.1.0.0",
+    "category": "Prueba tecnica",
+    "author": "Enmanuel Ferrer",
+    "contributor": [
+        "https://github.com/EnmanuelFerrer",
+    ],
+    "description": "Framework 101 - Estate Account Link Module",
+    "depends": ["estate", "account"],
+    "data": [],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
