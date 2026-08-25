14bf48a31289f5f8264aebf5602fdb1f2317c626
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-08-25 14:33:45 -0400

    feat(estate): add res.users view inheritance for property_ids

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -16,7 +16,7 @@
         "views/estate_property_offer_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
-        "views/inherited_model_views.xml",
+        "views/res_users_views.xml",
         "views/estate_property_menus.xml",
     ],
     "application": False,
